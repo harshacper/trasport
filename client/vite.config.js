97d49fd8251d0e1beff69d79ecc21b3675ee6231
harsha <harshasubhash123@gmail.com>
2026-08-10 20:00:59 +0530

    Change outputDirectory to client/dist and remove legacy version key from vercel.json

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: '../public',
-    emptyOutDir: true
-  }
+  plugins: [react()]
 })
